add98f6124c6552dfd4fd4f39552998a3d66e691
Santino Castro <gusabelu1@gmail.com>
2024-09-30 14:49:37 -0300

    Primer push

---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+-----------------------------------------------------------------------------------------------
+Título: Equipo 02 - TP
+Fecha: 23/09/2024
+Autor:
+Santino Castro
+
+Descripción:
+Sistema de gestión de productos, proveedores y clientes de un comercio electrónico dedicado 
+a la venta de componentes de computadoras.
+
+Pendientes:
+Marcar realizados con "X"
+
+1) Agregar CRUD con módulos:
+    - Productos
+    - Clientes
+    - Ventas
+
+2) Búsqueda por filtros
+
+3) Generar ventas modificando clientes y stock de productos a la par
+
+4) Descarga y monitoreo de stock mediante archivos (CSV)
+
+-----------------------------------------------------------------------------------------------
+"""
+
+#----------------------------------------------------------------------------------------------
+# MÓDULOS
+#----------------------------------------------------------------------------------------------
+import ventas
+
+
+#----------------------------------------------------------------------------------------------
+# FUNCIONES
+#----------------------------------------------------------------------------------------------
+...
+
+
+
+#----------------------------------------------------------------------------------------------
+# CUERPO PRINCIPAL
+#----------------------------------------------------------------------------------------------
+def main():
+    #-------------------------------------------------
+    # Inicialización de variables
+    #----------------------------------------------------------------------------------------------
+    ...
+
+
+    #-------------------------------------------------
+    # Bloque de menú
+    #----------------------------------------------------------------------------------------------
+    while True:
+        opciones = 5
+        while True:
+            print()
+            print("---------------------------")
+            print("MENÚ DEL SISTEMA           ")
+            print("---------------------------")
+            print("[1] Generar venta")
+            print("[2] Administrar inventario")
+            print("[3] Administar clientes")
+            print("[4] Historial de ventas")
+            print("---------------------------")
+            print("[0] Salir del programa")
+            print()
+            
+            opcion = input("Seleccione una opción: ")
+            if opcion in [str(i) for i in range(0, opciones)]: # Sólo continua si se elije una opcion de menú válida
+                break
+            else:
+                input("Opción inválida. Presione ENTER para volver a seleccionar.")
+        print()
+
+        if opcion == "0": # Opción salir del programa
+            exit() # También puede ser sys.exit() para lo cual hay que importar el módulo sys
+
+        elif opcion == "1":   # Generar Venta
+            print("Llamar a generar venta")
+            ventas.generarVenta()
+        elif opcion == "2":   # Administrar Inventario
+            print("Llamar a administrar inventario")
+        elif opcion == "3":   # Administrar Clientes
+            print("Llamar a administrar clientes")
+        elif opcion == "4":   # Historial de Ventas
+            print("Llamar a historial de ventas")
+
+
+# Punto de entrada al programa
+main()
---
+++ ventas.py
@@ -0,0 +1,68 @@
+def menuProductos(productos):
+    opciones = len(productos)
+    listaProductos = []
+
+    while True:
+        print()
+        print("---------------------------")
+        print("SELECCIONE LOS PRODUCTOS")
+        print("---------------------------")
+        for i in range(opciones):
+            print(f"[{i+1}] {productos[i]}")
+        print("---------------------------")
+        print("[0] Volver a las categorías")
+        print()
+        
+        opcion = input("Seleccione una opción: ")
+        if opcion in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+            if opcion == "0":
+                break
+            else:
+                for i in range(opciones):
+                    if i+1 == int(opcion):
+                        listaProductos.append(productos[i])
+        else:
+            input("Opción inválida. Presione ENTER para volver a seleccionar.")
+    print()
+
+    return listaProductos
+
+def generarVenta():
+    productos = [
+        {"id": 1, "nombre": "Monitor", "stock": 10, "precio": 500, "categoria": "monitor"},
+        {"id": 2, "nombre": "Teclado", "stock": 5, "precio": 100, "categoria": "teclado"},
+    ]
+
+    print("---------------------------")
+    print("GENERAR VENTA")
+    print("---------------------------")
+
+    cliente = input("Ingrese el nombre del cliente: ")
+    listaProductos = []
+    print("")
+
+    opciones = 2
+    while True:
+        print("---------------------------")
+        print("CATEGORIAS DE PRODUCTOS")
+        print("---------------------------")
+        print("[1] Monitores")
+        print("---------------------------")
+        print("[0] Cancelar")
+        print("[X] Generar Venta")
+        print()
+
+        opcion = input("Seleccione una opción: ")
+        if opcion == "X" or opcion == "x":
+            print(listaProductos)
+            break
+        
+        if opcion in [str(i) for i in range(0, opciones)]: # Sólo continua si se elije una opcion de menú válida
+            if opcion == "0": # Opción salir del programa
+                break
+            elif opcion == "1":   # Generar Venta
+                print("Menu de Monitores")
+                listaProductos.append(menuProductos([producto for producto in productos if producto['categoria'] == 'monitor']))
+        else:
+            input("Opción inválida. Presione ENTER para volver a seleccionar.")
+    print()
